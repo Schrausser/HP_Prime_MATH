902d207b3200fa5d5d773923c120f0cfe4971bd9
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-13 16:21:14 +0100

    Create V00.pas

---
+++ V00.pas
@@ -0,0 +1,17 @@
+//V00()/D.G.SCHRAUSSER/2022
+//Advanced Graphing: Clear
+EXPORT V00()
+BEGIN
+"Y=0"▶V1;
+"Y=0"▶V2;
+"Y=0"▶V3;
+"Y=0"▶V4;
+"Y=0"▶V5;
+"Y=0"▶V6;
+"Y=0"▶V7;
+"Y=0"▶V8;
+"Y=0"▶V9;
+"Y=0"▶V0;
+"Advanced Graphing: Clear!"
+END;
+//
