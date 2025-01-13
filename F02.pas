d1eb262334c5a498f44b4ac3884c8ce27aa122e6
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-13 13:58:44 +0100

    Create F02.pas

---
+++ F02.pas
@@ -0,0 +1,17 @@
+//F02()/D.G.SCHRAUSSER/2022
+//Function: Equations 2.0
+EXPORT F02()
+BEGIN
+"NORMALD_CDF(0,1,X)"▶F1;
+"NORMALD(0,1,X)"▶F2;
+"STUDENT(50,X)"▶F3;
+"STUDENT_CDF(50,X)"▶F4;
+"CHISQUARE(1,X)"▶F5;
+"CHISQUARE_CDF(1,X)"▶F6;
+"FISHER_CDF(25,3,X)"▶F7;
+"FISHER(25,3,X)"▶F8;
+"0"▶F9;
+"0"▶F0;
+"Function: Equations 2.0"
+END;
+//
