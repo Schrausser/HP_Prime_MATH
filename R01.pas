80bc3ad003acd0455c1321dc9db9f2f84bd9d8a0
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-13 14:11:49 +0100

    Create R01.pas

---
+++ R01.pas
@@ -0,0 +1,17 @@
+//R01()/D.G.SCHRAUSSER/2022
+//Polar: Polar function equations
+EXPORT R01()
+BEGIN
+"8*SIN(6*θ)"▶R1;
+"2*COS(8*θ)"▶R2;
+"θ^2"▶R3;
+"√(30-θ^2)"▶R4;
+"−1*√(30-θ^2)"▶R5;
+"√(20-θ^2)"▶R6;
+"−1*√(20-θ^2)"▶R7;
+"θ"▶R8;
+""▶R9;
+""▶R0;
+"Polar: Polar function equations"
+END;
+//
