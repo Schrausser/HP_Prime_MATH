359fda385f7ec11d113a849af59408b849e887f2
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-13 15:58:23 +0100

    Create X01.pas

---
+++ X01.pas
@@ -0,0 +1,17 @@
+//X01()/D.G.SCHRAUSSER/2022
+//Parametric: Parametric function equations
+EXPORT X01()
+BEGIN
+"SIN(T)*3"▶X1;
+"(T/4)-5"▶Y1;
+"SIN(T)*2"▶X2;
+"COS(T)*4"▶Y2;
+"T^2-5"▶X3;
+"T"▶Y3;
+"T-5"▶X4;
+"−1*(√(T))"▶Y4;
+""▶X5;
+""▶Y5;
+"Parametric: Parametric function equations"
+END;
+//
