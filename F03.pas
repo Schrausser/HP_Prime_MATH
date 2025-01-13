6632d41985cc7dc81eb38b30378fc925c4b73baa
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-13 14:00:15 +0100

    Create F03.pas

---
+++ F03.pas
@@ -0,0 +1,17 @@
+//F03()/D.G.SCHRAUSSER/2022
+//Function: Equations 3.0
+EXPORT F03()
+BEGIN
+"NORMALD_CDF(0,1,X)"▶F1;
+"NORMALD(0,1,X)"▶F2;
+"∂((1/√(2*π))*e^((-1/2)*X^2),X=X)"▶F3;
+"∂(∂((1/√(2*π))*e^((-1/2)*X^2),X),X)"▶F4;
+"0"▶F5;
+"0"▶F6;
+"0"▶F7;
+"0"▶F8;
+"0"▶F9;
+"0"▶F0;
+"Function: Equations 3.0"
+END;
+//
