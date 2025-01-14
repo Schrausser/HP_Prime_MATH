f6c813cb440836bc8246ae2537b71db1d970aa6a
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-14 17:10:13 +0100

    Create CPLX2.pas

---
+++ CPLX2.pas
@@ -0,0 +1,14 @@
+//CPLX2(complex number,a+bi)/D.G.SCHRAUSSER/2022
+//e.g.CPLX2(2+i/2),[Spreadsheet]//
+#cas
+CPLX2(C):=
+BEGIN
+CPLX(C)
+STARTAPP("Arbeitsblatt");
+"z"▶A1;Z1▶B1
+"|z|"▶A2;L1(1)▶B2
+"∡π"▶A3;L1(2)▶B3
+"∡°"▶A4;L1(3)▶B4
+END;
+#end
+//
