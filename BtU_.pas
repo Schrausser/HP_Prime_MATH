51b5b426fb6b7d75587fbe3098ba1240457f5d5d
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-15 14:15:46 +0100

    Create BtU_.pas

---
+++ BtU_.pas
@@ -0,0 +1,50 @@
+
+//BtU_(simulation cycles B)/D.G.SCHRAUSSER/2025
+//Bootstrap method, Bt
+//2 independent samples (x|g)
+//e.g.BtU_(1000)
+#cas
+BtU_(B):=
+BEGIN
+//L1L2 provided
+{}▶L3;{}▶L4
+{}▶L5;{}▶L6
+0▶M11
+0▶M12
+0▶M2
+SIZE(L1)▶N1
+SIZE(L2)▶N2
+N=N1+N2
+ABS(mean(L1)-mean(L2))▶Q02
+ΣLIST(L1)▶Q011
+ΣLIST(L2)▶Q012
+//
+CONCAT(L1,L2)▶L9
+MSGBOX("BtU")
+FOR J FROM 1 TO B 
+DO
+//
+FOR A FROM 1 TO N DO
+L9(RANDINT(N))▶L0(A) END;
+FOR A FROM 1 TO  N1 DO
+L0(A)▶L3(A) END;
+FOR A FROM 1 TO  N2 DO
+L0(N1+A)▶L4(A) END;
+ABS(mean(L3)-mean(L4))▶QJ2
+ΣLIST(L3)▶QJ11
+ΣLIST(L4)▶QJ12
+IF QJ11≥Q011 THEN M11=M11+1 END;
+IF QJ12≥Q012 THEN M12=M12+1 END;
+IF QJ2≥Q02 THEN M2=M2+1 END;
+QJ11▶L5(J)
+QJ2▶L6(J)
+END;
+//
+SORT(L5)▶L5
+SORT(L6)▶L6
+{}▶L9
+{}▶L0
+N1,N2,[Q011,Q012,Q02],M11/B,M12/B,M2/B
+END;
+#end
+//
