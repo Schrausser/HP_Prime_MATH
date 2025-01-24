0e35b20412cde09bb254cf87ec76dd7c1ee7a523
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-24 16:25:35 +0100

    Update RHO.pas

--- RHO.pas
+++ RHO.pas
@@ -9,7 +9,7 @@ mean(L1)▶L3
 MAKELIST((L1(I)(1)-L1(I)(2))^2,I,1,N)▶L2
 Σ(L2)▶SUM
 RHO=1-((6*SUM)/(N*(N^2-1)))
-KR=approx((2/(N/2))*(L3(1)-L3(2)))
+KR=approx((2/N)*(L3(1)-L3(2)))
 //n,rho,rbisR,r
 approx(N,[RHO,1-KR,correlation(L1)])
 END;
