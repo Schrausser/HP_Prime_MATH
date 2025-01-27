6a52e7e9db95b44b7e237598ca6811cfb7d0e64e
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-28 00:13:00 +0100

    Update TAU.pas

--- TAU.pas
+++ TAU.pas
@@ -11,6 +11,7 @@ IF L3(I)=1 THEN 0▶L3(I) END;
 IF L3(I)=-1 THEN 1▶L3(I) END;
 888▶M1(L1(I),L2(I))
 END;
+888▶M1(L1(N),L2(N))
 //(N*(N-1)/2-2*ΣLIST(L3))/(N*(N-1)/2)▶tau_a
 approx(1-2*ΣLIST(L3)/(N*(N-1)/2))▶L4(1)
 approx((√(N*(N-1)))/(√(2*(2*N+5)))*3*L4(1))▶L4(2)
@@ -18,10 +19,10 @@ NORMALD_CDF(L4(2))▶L4(3)
 1-L4(3)▶L4(4)
 L4(4)*2▶L4(5)
 IF L4(3)<0.5 THEN 2*L4(3)▶L4(5)
+((COMB(N,2)-ΣLIST(L3))-ΣLIST(L3))/√((1/18)*N*(N-1)*(2*N+5))▶L4(6)
 END;
 //taua,z,p,1-p,p2
 [L4(1)],[L4(2)],L4(3),L4(4),[L4(5)]
-//M1
 END;
 #end
 //
