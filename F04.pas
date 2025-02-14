0e0805fa595f3a08f795b630c9d1e36864df8118
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-02-14 11:38:37 +0100

    Create F04.pas

---
+++ F04.pas
@@ -0,0 +1,17 @@
+//F04()/D.G.SCHRAUSSER/2025
+//Function: Equations 4.0
+EXPORT F04()
+BEGIN
+"CAS.Gamma(X)"▶F1;
+"(X)!"▶F2;
+"∂(Gamma(X),X=X)"▶F3;
+"EXP(X)"▶F4;
+"∂(e^X,X = X)"▶F5;
+"∂(∂(e^X,X=X),X=X)"▶F6;
+"∂(∂(∂(e^X,X=X),X=X),X=X)"▶F7;
+"0"▶F8;
+"0"▶F9;
+"0"▶F0;
+"Function: Equations 4.0"
+END;
+//
