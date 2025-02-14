7cf7565a854e2f75e4e281a42b0ffdb59d69d793
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-02-14 17:42:34 +0100

    Update AMG.pas

--- AMG.pas
+++ AMG.pas
@@ -1,15 +1,18 @@
 //AMG()/D.G.SCHRAUSSER/2025
-//Weighted arithmetic mean GAM
+//Weighted arithmetic, geometric and harmonic mean
 #cas
 AMG():=
 BEGIN
 //L1()(2) provided
-size(L1)▶N
-MAKELIST(L1(x)(1)*L1(x)(2),x,1,N)▶L2
-ΣLIST(L1)(2)▶N1
-Σ(L2)/N1▶GAM
-//
-N,mean(L1)(1),N1,[GAM]
+size(L1)▶L3(1)
+mean(L1)(1)▶L3(2)
+MAKELIST(L1(x)(1)*L1(x)(2),x,1,L3(1))▶L2
+ΣLIST(L1)(2)▶L3(3)
+Σ(L2)/L3(3)▶L3(4)
+L3(3) NTHROOT (product((L1(x)(1))^L1(x)(2),x,1,L3(1)))▶L3(5)
+L3(3)/(Σ(L1(x)(2)/L1(x)(1),x,1,L3(1)))▶L3(6)
+//n,AM,sumni,GAM,GGM,GHM
+L3
 END;
 #end
 //
