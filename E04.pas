61701ef2211d6067b986f40ea0d52053232bdcca
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-06-16 00:44:11 +0200

    Update E04.pas

--- E04.pas
+++ E04.pas
@@ -7,6 +7,7 @@
 //E5: Speed of light C from m/c M
 //E6: Luminosity distance P from distance modulus M
 //E7: Radius R at a given distance D with angular diameter V°
+//E8: Illuminance in lux E from apparent magnitude M
 EXPORT E04()
 BEGIN
 "A=M/149597870700"▶E1;
@@ -16,9 +17,9 @@ BEGIN
 "C=299792458*M"▶E5;
 "P=10^((M/5)+1)"▶E6;
 "R=D*TAN(((π/180)*V/2))"▶E7;
-""▶E8;
+"E=10^((-14.18-M)/2.5)"▶E8;
 ""▶E9;
 ""▶E0;
 "Solve: Equations 4.0"
 END;
-//
+// 
